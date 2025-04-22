7985b875fb2ac8acdef016d542d49c16af403894
Evelin <evelinmariabarroso@gmail.com>
2025-04-22 09:01:27 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+export default function App() {
+  return (
+    <div>
+     <img src="react.png" alt="" />
+     <h1>React</h1>
+     <p>A biblioteca para interfaces de usuários web e nativas.</p>
+     <button class='aprenda' id='aprenda'>Aprenda React</button>
+     <button class='referencia' id='referencia'>Referência da API</button>
+     <hr></hr>
+     <h2>Crie interfaces de usuário de componentes</h2>
+     <p>React permite que você construa interfaces d usuário a partir de pedaços individuais chamados componentes.</p>
+     <hr></hr>
+     <h3>Escreva componentes com códigos e marcação</h3>
+     <p>Componentes React são funções JavaScript.A sintaxe de marcação é chamada de JSX.É uma extensão da sintaxe JavaScript popularizada pelo React. </p>
+     <hr></hr>
+     <h4>Próximos passos</h4>
+     <li>
+      Uso de dados dinámicos
+      <li>
+
+      Criação de novos componetes
+      </li>
+      <li>
+
+      Estilização de componentes
+      </li>
+      <li>
+
+      Reutilização de componentes
+      </li>
+      <li>
+
+      Uso de props e children
+      </li>
+      <li>
+
+      Uso de eventos do  JavaScript
+      </li>
+     </li>
+    </div>
+  )
+}
